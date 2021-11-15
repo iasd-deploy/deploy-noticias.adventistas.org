b49630f4bb454e2c083c87ca5888a18dd17e0961
Eli Mendonça <contato@elimendonca.com>
2021-11-15 16:46:07 -0300

    - Ativa os filtros dos modulos do Jetpack - Ajuste no processamento das colunas

--- app/pt/processaColunas.sh
+++ app/pt/processaColunas.sh
@@ -3,7 +3,9 @@
 posts=$(wp post list --post_type=colunas --format=ids --allow-root)
 
 for i in ${posts[@]}; do
+	wp post meta add $i post_processed false --allow-root
 	echo "Processando post ID - $(wp post get $i --field=ID --allow-root) " 
 	wp post update $i --post_type=post --allow-root
 	wp post term add $i xtt-pa-format artigo --allow-root
+	wp post meta update $i post_processed true --allow-root
 done
