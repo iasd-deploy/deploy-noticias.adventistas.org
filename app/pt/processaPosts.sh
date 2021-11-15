2d49e0ef70fd381c93196dccde19f3cf45e9bd4e
Eli Mendonça <contato@elimendonca.com>
2021-11-15 17:50:39 -0300

    Altera o script de processamento dos posts

--- app/pt/processaPosts.sh
+++ app/pt/processaPosts.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-posts=$(wp post list --post_type=post --posts_per_page=10000 --meta_key="post_processed" --meta_value="false" --meta_compare="NOT EXISTS" --format=ids --allow-root)
-
-echo ${posts[@]}
+posts=$(wp post list --post_type=post --posts_per_page=$2 --paged=$1 --meta_key="post_processed" --meta_value="false" --meta_compare="NOT EXISTS" --format=ids --allow-root)
+CONTER=1
+# echo ${posts[@]}
 
 for p in ${posts[@]}; do
-   wp post meta add $p post_processed false --allow-root
-   echo "Processando post ID - $(wp post get $p --field=ID --allow-root) " 
-   # wp post update $p --post_type=post --allow-root
-   wp post term add $p xtt-pa-format noticia --allow-root
+   time wp post term add $p xtt-pa-format noticia --allow-root
+   echo " $CONTER - FIM do post ID - $p" 
+   CONTER=$[$CONTER + 1]
+   
    wp post meta update $p post_processed true --allow-root
 done
