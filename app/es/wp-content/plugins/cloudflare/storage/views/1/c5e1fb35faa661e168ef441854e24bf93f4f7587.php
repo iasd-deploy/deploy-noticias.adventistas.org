09d418624e24aa3cfdb434ab6597f5956d23c495
Eli Mendonça <contato@elimendonca.com>
2021-11-28 08:09:17 -0300

    - Atualiza plugins - Atualiza traduções - Altera a regra de rewrite das colunas

---
+++ app/es/wp-content/plugins/cloudflare/storage/views/1/c5e1fb35faa661e168ef441854e24bf93f4f7587.php
@@ -0,0 +1,10 @@
+<section class="pa-header py-3">
+    <header class="container">
+        <div class="row">
+            <div class="col py-5">
+                <span class="pa-tag rounded-1 px-3 py-1 d-inline-block"><?php echo e(__('Adventist news', 'iasd')); ?></span>
+                <h1 class="mt-2"><?= getHeaderTitle() ?></h1>
+            </div>
+        </div>
+    </header>
+</section><?php /**PATH /Users/eli/Dropbox (ComunicaDSA)/projects/deploy-noticias.adventistas.org/app/es/wp-content/themes/pa-theme-noticias/components/header/title.blade.php ENDPATH**/ ?>
---
+++ app/es/wp-content/plugins/cloudflare/storage/views/1/c8d2cd36ac171400ce736aebbb697600055e1b36.php
@@ -0,0 +1,40 @@
+<header class="mb-4">
+    <h1 class="fw-bold mb-3"><?php echo single_post_title(); ?></h1>
+
+    <h2 class="mb-3 pb-3"><?php echo \Illuminate\Support\Str::of(get_the_excerpt())->limit(250); ?></h3>
+
+    <?php if(!empty($format = getPostFormat(get_the_ID())) && $format->slug != 'artigo' && is_singular('post')): ?>        
+        <div class="pa-post-meta mb-2"><?php echo e(__('By', 'iasd')); ?> 
+            <span><?php echo !empty($custom_author = get_field('custom_author')) ? $custom_author : get_the_author(); ?></span><?php if($region = getPostRegion(get_the_ID())): ?><em class="pa-pipe">|</em><span class="ms-2"><i class="fas fa-map-marker-alt me-2" aria-hidden="true"></i> <?php echo e($region->name); ?></span><?php endif; ?>
+        </div>
+    <?php endif; ?>
+
+    <div class="pa-post-meta"><?php echo the_date(); ?></div>
+
+    <hr class="my-45">
+
+    <div class="d-flex justify-content-between">
+        <div class="pa-share d-none d-xl-block">
+            <?php require(get_template_directory() . '/components/parts/share.php') ?>
+        </div>
+
+        <div class="">
+            <ul class="pa-accessibility list-inline">
+                <li class="pa-text-dec list-inline-item"><a href="#" class="rounded p-2" onclick="window.TextSize.pa_diminui_texto(event)">-A</a></li>
+                <li class="pa-text-inc list-inline-item"><a href="#" class="rounded p-2" onclick="window.TextSize.pa_aumenta_texto(event)">+A</a></li>
+
+                <?php if(get_post_meta(get_the_ID(), 'amazon_polly_enable', true)): ?>
+                    <li class="pa-text-listen list-inline-item">
+                        <a href="#" class="rounded p-2" onclick="pa_play(event, this)">
+                            <i class="fas fa-volume-up"></i> <?php echo e(__('Hear text', 'iasd')); ?>
+
+                        </a>
+                        
+                        <audio id="pa-accessibility-player" src="<?php echo e(get_post_meta(get_the_ID(), 'amazon_polly_audio_link_location', true)); ?>" controls></audio>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
+</header>
+<?php /**PATH /Users/eli/Dropbox (ComunicaDSA)/projects/deploy-noticias.adventistas.org/app/es/wp-content/themes/pa-theme-noticias/template-parts/single/header.blade.php ENDPATH**/ ?>
